78b1c277ce8e277186e70bf30d1f15d994f1de8c
marcello <mvlservicosinfo@gmail.com>
2025-01-09 15:07:11 -0300

    "Arquivos de Exercicios em Python"

---
+++ Exercicio_001/Funcoes/Operadores.py
@@ -0,0 +1,20 @@
+class operador():
+    def tiposOperadores(self):
+        n1=100
+        n2=200
+        n3=300
+        n4=400
+        n5=500
+        n6=88
+
+        print("-"*30)
+        op1 =n1/n2
+        op2 = n2//n1
+        op3 = n1%n2
+        op4 = n4//n5
+        op5 = n5 / n6
+        print("OP1 = ",op1)
+        print("OP2 = ", op2)
+        print("OP3= ", op3)
+        print("OP4 = ", op4)
+        print("OP5 = ", op5)
