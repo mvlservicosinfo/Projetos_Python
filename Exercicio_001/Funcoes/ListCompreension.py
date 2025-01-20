fc605e742be261637a0b98503d06fc8748e485a9
marcello <mvlservicosinfo@gmail.com>
2025-01-20 09:43:33 -0300

    "Funcoes de ListCompreension"

--- Exercicio_001/Funcoes/ListCompreension.py
+++ Exercicio_001/Funcoes/ListCompreension.py
@@ -1,2 +1,10 @@
 class Lista():
+
+    def __init__(self):
+        pass
+
     calc = [x for x in range(10)]
+
+    dict_mercado = {'laranja':18.00,'Batata':9.40, 'Mamao':5.00}
+    dict_economia ={k:('Tá Barato' if c <18.00 else 'Tá Caro') for (k,c) in dict_mercado.items()}
+
