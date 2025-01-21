11968d765f4dc10a6e4bc7247db6452ad821f3e1
marcello <mvlservicosinfo@gmail.com>
2025-01-21 09:08:03 -0300

    "ListCompressao"

--- Exercicio_001/Funcoes/ListCompreension.py
+++ Exercicio_001/Funcoes/ListCompreension.py
@@ -5,6 +5,8 @@ class Lista():
 
     calc = [x for x in range(10)]
 
-    dict_mercado = {'laranja':18.00,'Batata':9.40, 'Mamao':5.00}
+    dict_mercado = {'laranja':18.00,'Batata':9.40, 'Mamao':5.00, 'Uva': 14.66}
     dict_economia ={k:('Tá Barato' if c <18.00 else 'Tá Caro') for (k,c) in dict_mercado.items()}
+
+
 
